46cf51d6cc212a997f9c0f35ef2480ae2546c42e
baegopa <baegopa@wamwam.com>
2020-08-25 12:51:17 +0900

    200825 spring instagram - 6. 로그인 완료

--- src/main/java/com/cos/instagram/controller/IndexController.java
+++ src/main/java/com/cos/instagram/controller/IndexController.java
@@ -2,6 +2,8 @@ package com.cos.instagram.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
@@ -10,39 +12,40 @@ public class IndexController {
 	public String test1() {
 		return "auth/login";
 	}
-	
+
 	@GetMapping("/test/join")
 	public String test2() {
 		return "auth/join";
 	}
-	
+
 	@GetMapping("/test/following")
 	public String test3() {
 		return "follow/following";
 	}
-	
+
 	@GetMapping("/test/explore")
 	public String test4() {
 		return "image/explore";
 	}
-	
+
 	@GetMapping("/test/feed")
 	public String test5() {
 		return "image/feed";
 	}
-	
+
 	@GetMapping("/test/upload")
 	public String test6() {
 		return "image/image-upload";
 	}
-	
+
 	@GetMapping("/test/proedit")
 	public String test7() {
 		return "user/profile-edit";
 	}
-	
+
 	@GetMapping("/test/profile")
 	public String test8() {
-		return "user/profile"; 
+		return "user/profile";
 	}
+
 }
---
+++ src/main/java/com/cos/instagram/web/dto/LoginReqDto.java
@@ -0,0 +1,9 @@
+package com.cos.instagram.web.dto;
+
+import lombok.Data;
+
+@Data
+public class LoginReqDto {
+	private String username;
+	private String password;
+}
